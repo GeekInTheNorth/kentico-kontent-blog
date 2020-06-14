e70e4b858d37232989f33b5ac4219c13cb806eb2
Mark Stott <mypantsareontheoutside@gmail.com>
2020-06-14 15:48:21 +0100

    Wire Up an initial article controller.

---
+++ src/KenticoKontentBlog/KenticoKontentBlog/Controllers/ArticleController.cs
@@ -0,0 +1,40 @@
+﻿using Kentico.Kontent.Delivery;
+using KenticoKontentBlog.KenticoModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace KenticoKontentBlog.Controllers
+{
+    public class ArticleController : Controller
+    {
+        private readonly ILogger<ArticleController> _logger;
+
+        private readonly IConfiguration _configuration;
+
+        public ArticleController(ILogger<ArticleController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        [Route("article/{articleStub}")]
+        public IActionResult Index(string articleStub)
+        {
+            try
+            {
+                var client = DeliveryClientBuilder.WithProjectId(_configuration["KenticoKontent:DeliveryApi"]).Build();
+
+                var model = client.GetItemAsync<BlogArticle>(articleStub).Result.Item;
+
+                return View(model);
+            }
+            catch (Exception exception)
+            {
+                _logger.LogError("Oops", exception);
+                throw;
+            }
+        }
+    }
+}
---
+++ src/KenticoKontentBlog/KenticoKontentBlog/KenticoModels/BlogArticle.cs
@@ -0,0 +1,42 @@
+﻿using Kentico.Kontent.Delivery.Abstractions;
+using System.Collections.Generic;
+
+namespace KenticoKontentBlog.KenticoModels
+{
+    public partial class BlogArticle
+    {
+        public const string Codename = "blogarticle";
+        public const string ArticleContentCodename = "article_content";
+        public const string HeaderImageCodename = "header_image";
+        public const string ImageCarouselCodename = "image_carousel";
+        public const string RelatedArticlesCodename = "related_articles";
+        public const string SeoMetaDataSeoDescriptionCodename = "seo_meta_data__seo_description";
+        public const string SeoMetaDataSeoImageCodename = "seo_meta_data__seo_image";
+        public const string SeoMetaDataSeoTitleCodename = "seo_meta_data__seo_title";
+        public const string SeoMetaDataTwitterAccountNameCodename = "seo_meta_data__twitter_account_name";
+        public const string TitleCodename = "title";
+        public const string UrlSlugCodename = "url_slug";
+
+        public string ArticleContent { get; set; }
+
+        public IEnumerable<Asset> HeaderImage { get; set; }
+
+        public IEnumerable<Asset> ImageCarousel { get; set; }
+
+        public IEnumerable<object> RelatedArticles { get; set; }
+
+        public string SeoMetaDataSeoDescription { get; set; }
+
+        public IEnumerable<Asset> SeoMetaDataSeoImage { get; set; }
+
+        public string SeoMetaDataSeoTitle { get; set; }
+
+        public IEnumerable<MultipleChoiceOption> SeoMetaDataTwitterAccountName { get; set; }
+
+        public ContentItemSystemAttributes System { get; set; }
+
+        public string Title { get; set; }
+
+        public string UrlSlug { get; set; }
+    }
+}
