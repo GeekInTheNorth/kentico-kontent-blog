cb3e25f36e42c2f1dac7acb6a5b802043866269c
Mark Stott <mypantsareontheoutside@gmail.com>
2020-08-20 23:04:18 +0100

    Add listing pages to the xml sitemap

--- src/KenticoKontentBlog.Feature/SiteMap/SiteMapBuilder.cs
+++ src/KenticoKontentBlog.Feature/SiteMap/SiteMapBuilder.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
+using Kentico.Kontent.Delivery;
 using Kentico.Kontent.Delivery.Abstractions;
 
 using KenticoKontentBlog.Feature.Framework;
@@ -37,19 +39,40 @@ namespace KenticoKontentBlog.Feature.SiteMap
                 LastModified = DateTime.Today
             });
 
+            var articleListings = await GetArticleListings();
+            nodes.AddRange(articleListings);
+
+            var articles = await GetArticles();
+            nodes.AddRange(articles);
+
+            return nodes;
+        }
+
+        private async Task<IEnumerable<SitemapNode>> GetArticleListings()
+        {
+            var siteSettingsResponse = await deliveryClient.GetItemsAsync<SiteSettings>(new LimitParameter(1), new OrderParameter($"system.last_modified", SortOrder.Descending));
+            var siteSettings = siteSettingsResponse.Items.FirstOrDefault();
+
+            return siteSettings.Category.Select(x => new SitemapNode
+            {
+                Url = urlHelper.Action(Globals.Routing.List, Globals.Routing.ArticleController, new { category = x.Codename }, Globals.Routing.DefaultProtocol),
+                LastModified = DateTime.Today
+            });
+        }
+
+        private async Task<IEnumerable<SitemapNode>> GetArticles()
+        {
+            var nodes = new List<SitemapNode>();
             var contentFeed = deliveryClient.GetItemsFeed<ArticlePage>();
             while (contentFeed.HasMoreResults)
             {
                 var batch = await contentFeed.FetchNextBatchAsync();
 
-                foreach(var contentItem in batch.Items)
+                nodes.AddRange(batch.Items.Select(x => new SitemapNode
                 {
-                    nodes.Add(new SitemapNode
-                    {
-                        Url = urlHelper.Action(Globals.Routing.Index, Globals.Routing.ArticleController, new { articleStub = contentItem.System.Codename }, Globals.Routing.DefaultProtocol),
-                        LastModified = contentItem.System.LastModified
-                    });
-                }
+                    Url = urlHelper.Action(Globals.Routing.Index, Globals.Routing.ArticleController, new { articleStub = x.System.Codename }, Globals.Routing.DefaultProtocol),
+                    LastModified = x.System.LastModified
+                }));
             }
 
             return nodes;
