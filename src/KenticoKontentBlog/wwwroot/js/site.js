b9938f290f52c1acee9e4986891b96fb9536bf3f
Mark Stott <mypantsareontheoutside@gmail.com>
2021-08-01 23:31:59 +0100

    Apply table styles within Rich Content Areas.

--- src/KenticoKontentBlog/wwwroot/js/site.js
+++ src/KenticoKontentBlog/wwwroot/js/site.js
@@ -1,4 +1,3 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿$(document).ready(function () {
+    $("div.js-content-container > table").addClass("table table-striped");
+});
