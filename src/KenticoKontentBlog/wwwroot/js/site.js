5cc050420a9496ce8210228ff7bc7c3d00925561
Mark Stott <mypantsareontheoutside@gmail.com>
2021-08-05 23:14:39 +0100

    Further tables tweaks within Rich Text Areas.

--- src/KenticoKontentBlog/wwwroot/js/site.js
+++ src/KenticoKontentBlog/wwwroot/js/site.js
@@ -1,3 +1,18 @@
 ﻿$(document).ready(function () {
-    $("div.js-content-container > table").addClass("table table-striped");
+    formatRichTextTables();
 });
+
+function formatRichTextTables() {
+    $('div.js-content-container > table').addClass('table table-striped');
+
+    $('<thead></thead>').prependTo('div.js-content-container > table');
+    $('<tr></tr>').prependTo('div.js-content-container > table thead').append(
+        $('td', 'div.js-content-container > table tbody tr:first').map(
+            function (i, e) {
+                return $('<th scope="col">').html(e.textContent).get(0);
+            }
+        )
+    );
+
+    $('div.js-content-container > table tbody tr:first').remove();
+}
